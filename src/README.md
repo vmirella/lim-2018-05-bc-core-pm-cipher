3be10f55e59d5c197c17e9588cbf2e334919ae3b
vmirella <vmirella_17@hotmail.com>
2018-05-23 18:15:10 -0500

    He hechos cambios en encode

---
+++ src/README.md
@@ -0,0 +1,29 @@
+# Definición del producto
+
+En el `README.md` cuéntanos cómo pensaste en los usuarios al desarrollar tu
+producto y cuál fue tu proceso para definir el producto final a nivel de
+experiencia y de interfaz.
+
+La distribución de los objetos en el producto ha sido hecha de manera que el usuario pueda usarlos de 
+manera intuitiva para ello también aparecerán textos  descriptivos que indiquen donde debe ingresar 
+el usuario sus textos.
+
+![alt GUI de cifrado C&eacute;sar] (https://imageshack.com/a/img921/2868/6CDjAk.png)
+
+
+* quiénes son los principales usuarios de producto
+
+Los principales usuarios del producto son los invitados a la fiesta sorpresa y yo.
+
+* cuáles son los objetivos de estos usuarios en relación con el producto
+
+El objetivo de los usuarios en relación al producto es poder comunicarse sin ser descubiertos
+por el cumpleañero.
+
+* cómo crees que el producto que estás creando les está resolviendo sus
+  problemas
+
+El producto resuelve sus problemas ya que hace que sus mensajes sean inintelegibles para el cumpleañero
+como para cualquiera que los lee y no sabe de la fiesta sorpresa, como lo hace les permite encodificar
+sus mensajes y también les permite decodificarlos usando el cifrado Cesar y dándoles la opción de elegir
+con cuantos desplazamientos este se aplicará.
--- src/cipher.js
+++ src/cipher.js
@@ -1,33 +1,42 @@
 //referencia: https://knpuniversity.com/screencast/javascript/window-global-vars
 //cuarta ventana negra de codigo
 window.cipher = {
-  //Funcion que codifica, recibe un caracter y la cantidad de desplazamientos.
-  encode: function(character, offset){
+  //Funcion que codifica, recibe un texto y la cantidad de desplazamientos.
+  encode: function(text, offset){
+    //Trasformo el texto a mayusculas
+    let textUpperCase = text.toUpperCase();
+    //Separar el texto letra por letra
+    let i;
+    let result ="";
+    for (i = 0, l=textUpperCase.length; i < l; i++) {
+      
+      result += textUpperCase[i] + " ";
+    }
     //Trasformo el caracter en ASCII
-    var characterAscii = character.charCodeAt(0);
+    //let characterAscii = character.charCodeAt(0);
     /*Le resto 65 para tener su posicion en abecedario,
      *le sumo el numero de posiciones a desplazarce para que tome el nuevo valor,
      *Lo divido entre 26 y tomo el residuo (se divide entre 26 que es el numero de letras del abecedario para ahorrarse el recorrido atravez de este)*/
-    var valueEncode = (characterAscii - 65 + offset) % 26 + 65;
+    //let valueEncode = (characterAscii - 65 + offset) % 26 + 65;
     //Volver a convertir el ASCII en letra
-    var characterEncode = String.fromCharCode(valueEncode);
+    //let characterEncode = String.fromCharCode(valueEncode);
     //Retorna la letra encodificada
-    return characterEncode;
+    return result;
   },
-  decode: function(character,offset){
+  decode: function(character,offset) {
     //Trasformo el caracter en ASCII
-    var characterAscii = character.charCodeAt(0);
+    let characterAscii = character.charCodeAt(0);
     /*Le resto 65 para tener su posicion en abecedario,
      *le resto el numero de posiciones a desplazarce para que tome el nuevo valor,
      *Lo divido entre 26 y tomo el residuo (se divide entre 26 que es el numero de letras del abecedario para ahorrarse el recorrido atravez de este)*/
-    var valueDecode = (characterAscii - 65 - offset) % 26 + 65;
+    let valueDecode = (characterAscii - 65 - offset) % 26 + 65;
      //Volver a convertir el ASCII en letra
-     var characterDecode = String.fromCharCode(valueDecode);
+     let characterDecode = String.fromCharCode(valueDecode);
      //Retorna la letra decodificada
     return characterDecode;
   }
 };
-console.log(window.cipher.encode('Z', 5));
-console.log(window.cipher.decode('D', 3));
-var texto = document.getElementById('texto').value;
-document.getElementById('texto').value = 'Hola';
+console.log(window.cipher.encode('esther', 5));
+//console.log(window.cipher.decode('D', 3));
+//var texto = document.getElementById('texto').value;
+//document.getElementById('texto').value = 'Hola';
