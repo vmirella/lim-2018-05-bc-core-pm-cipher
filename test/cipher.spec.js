4700450f7b2b674007781af10dc8fb2866bc2453
unknown <vmirella_17@hotmail.com>
2018-05-31 02:56:57 -0500

    Interfaz terminada

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,7 +10,6 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    //it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
       assert.equal(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
@@ -23,7 +22,6 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    //it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
       assert.equal(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG'), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     });
@@ -42,7 +40,6 @@ describe('cipher', () => {
     it('deberia retornar un objeto con la propiedad decode', ()=>{
       assert.property(cipher.createCipherWithOffset(), 'decode');
     })
-      //assert.equal(cipher.createCipherWithOffset(33), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
   });
 
 });
